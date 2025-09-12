f77c330eccd8cf8ceb0d6445d66b732ca59ad242
Arif Tamboli <arif.tamboli_6361@nihilent.com>
2025-09-12 06:04:05 +0200

    latest changes

--- Nihilent Dot net training/Day 3/Banking solution/BankingApp/obj/Debug/net8.0/BankingApp.AssemblyInfo.cs
+++ Nihilent Dot net training/Day 3/Banking solution/BankingApp/obj/Debug/net8.0/BankingApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankingApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e49064bfaa02a81b3fb4985ecfe95eafa670400f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c4372f279d537c294180ab9ff4119a0c59d8619")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankingApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankingApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Nihilent Dot net training/Day 5/VSEcommerceSolution/TestApp/obj/Debug/net5.0/TestApp.AssemblyInfo.cs
+++ Nihilent Dot net training/Day 5/VSEcommerceSolution/TestApp/obj/Debug/net5.0/TestApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e49064bfaa02a81b3fb4985ecfe95eafa670400f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c4372f279d537c294180ab9ff4119a0c59d8619")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
